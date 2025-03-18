0335497d960069ef59c3716381b017f137d4afe0
HvdW <117818214+HvdW144@users.noreply.github.com>
2025-03-18 14:04:12 +0100

    Create HANStack.java

---
+++ startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
@@ -0,0 +1,31 @@
+﻿package nl.han.ica.datastructures;
+
+import java.util.LinkedList;
+
+public class HANStack<T> implements IHANStack<T> {
+    private final LinkedList<T> stack;
+
+    public HANStack() {
+        stack = new LinkedList<>();
+    }
+
+    @Override
+    public void push(T value) {
+        int size = stack.size();
+        stack.add(size, value);
+    }
+
+    @Override
+    public T pop() {
+        int size = stack.size();
+        T value = stack.get(size - 1);
+        stack.remove(size - 1);
+        return value;
+    }
+
+    @Override
+    public T peek() {
+        int size = stack.size();
+        return stack.get(size - 1);
+    }
+}
