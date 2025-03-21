5beb94328d2d0b454c26818ad4a9cc09135cdd63
HvdW <117818214+HvdW144@users.noreply.github.com>
2025-03-21 18:56:00 +0100

    started on Checker

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,20 +1,20 @@
 package nl.han.ica.icss.checker;
 
-import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.AST;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
-
+import java.util.LinkedList;
 
 
 public class Checker {
 
-    private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    // teacher said we could use a normal linked list for this, so I changed it
+    private LinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
-
+        variableTypes = new LinkedList<>();
+        System.out.println(ast.root.getChildren());
     }
 
     
