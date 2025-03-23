1e7e6ae8354afc7977b48fd062d28689f0419351
HvdW <117818214+HvdW144@users.noreply.github.com>
2025-03-23 12:26:51 +0100

    started on checker

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,10 +1,11 @@
 package nl.han.ica.icss.checker;
 
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Objects;
 
 
 public class Checker {
@@ -15,7 +16,49 @@ public class Checker {
     public void check(AST ast) {
         variableTypes = new LinkedList<>();
         System.out.println(ast.root.getChildren());
+
+        checkNode(ast.root);
     }
 
-    
+    private void checkNode(ASTNode node) {
+        if (node instanceof Stylerule) {
+            checkStylerule((Stylerule) node);
+        } else if (node instanceof VariableAssignment) {
+            checkVariableAssignment((VariableAssignment) node);
+        } else if (node instanceof VariableReference) {
+            checkVariableReference((VariableReference) node);
+        } else if (node instanceof Declaration) {
+            checkDeclaration((Declaration) node);
+        } else if (node instanceof Operation) {
+            checkOperation((Operation) node);
+        } else if (node instanceof IfClause) {
+            checkIfClause((IfClause) node);
+        } else {
+            System.out.println("Unknown node type: " + node.getClass().getName());
+        }
+    }
+
+    private void checkStylerule(Stylerule node) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    private void checkVariableAssignment(VariableAssignment node) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    private void checkVariableReference(VariableReference node) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    private void checkDeclaration(Declaration node) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    private void checkOperation(Operation node) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    private void checkIfClause(IfClause node) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
 }
