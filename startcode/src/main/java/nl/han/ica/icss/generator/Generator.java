625bf34b51b6a42a788c5830d7d7c5f1d4638ec9
HvdW <117818214+HvdW144@users.noreply.github.com>
2025-03-31 13:54:02 +0200

    started on generator

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,15 +1,72 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.selectors.ClassSelector;
+import nl.han.ica.icss.ast.selectors.IdSelector;
+import nl.han.ica.icss.ast.selectors.TagSelector;
 
 public class Generator {
 
-	public String generate(AST ast) {
+    public String generate(AST ast) {
+        System.out.println(generateNode(ast.root));
+        return generateNode(ast.root);
+    }
+
+    private String generateNode(ASTNode node) {
+        StringBuilder resultString = new StringBuilder();
+
+        if (node instanceof Stylesheet) {
+            resultString.append(generateStylesheet((Stylesheet) node));
+        } else if (node instanceof Stylerule) {
+            resultString.append(generateStylerule((Stylerule) node));
+        } else if (node instanceof Declaration) {
+            return generateDeclaration((Declaration) node);
+        } else if (node instanceof PropertyName) {
+            return generatePropertyName((PropertyName) node);
+        } else if (node instanceof Expression) {
+            return generateExpression((Expression) node);
+        } else if (node instanceof Selector) {
+            resultString.append(generateSelectorNode((Selector) node));
+        }
+
+        return resultString.toString();
+    }
+
+    private String generateStylesheet(Stylesheet node) {
+        StringBuilder resultString = new StringBuilder();
+        for (ASTNode child : node.getChildren()) {
+            resultString.append(generateNode(child));
+        }
+        return resultString.toString();
+    }
+
+    private String generateStylerule(Stylerule node) {
         return "";
+    }
+
+    private String generateDeclaration(Declaration node) {
+        return "";
+    }
+
+    private String generatePropertyName(PropertyName node) {
+        return "";
+    }
+
+    private String generateExpression(Expression node) {
+        return "";
+    }
+
+    private String generateSelectorNode(Selector node) {
+        if (node instanceof TagSelector) {
+            return ((TagSelector) node).tag;
+        } else if (node instanceof IdSelector) {
+            return "#" + ((IdSelector) node).id;
+        } else if (node instanceof ClassSelector) {
+            return "." + ((ClassSelector) node).cls;
+        }
+        throw new IllegalArgumentException("Unknown selector type: " + node.getClass().getName() + "at Generator");
+    }
 
 
-	}
-
-	
 }
